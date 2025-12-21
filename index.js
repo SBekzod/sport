ec0fae5fafe82ba18aa1744473b0bb0d23b06fff
martin <martin@comets.kr>
2025-12-21 22:54:59 +0900

    BRR: Initial Setup

---
+++ index.js
@@ -0,0 +1,7 @@
+// PACKAGES: CORE | EXTERNAL | FILE
+
+const fs = require("fs");
+const moment = require("moment");
+
+const currentTime = moment().format("HH:mm:ss");
+console.log("currentTime:", currentTime);
