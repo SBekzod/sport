a06b1a2f313c4198dc386544882a7b054d917f5e
martin <martin@comets.kr>
2025-12-21 23:19:53 +0900

    fix: modify something

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-// PACKAGES: CORE | EXTERNAL | FILE
-
-const fs = require("fs");
-const calculate = require("./calculate");
-
-calculate(20, 30);
-
-/*
- 
-GIT COMMANDS:
-  git init
-  git status
-
-  git add .
-  git status
-  git commit -m "feat/fix: Here comes message"
-
-*/
+const moment = require("moment");
